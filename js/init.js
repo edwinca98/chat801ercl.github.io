158fc918f18b2e6978000ee556c9683f00a034e7
edwinca98 <87714379+edwinca98@users.noreply.github.com>
2021-07-20 14:14:52 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDD8xTz_V7NQnasjZAkMSZprnPyjEX6jvo",
+    authDomain: "chatedwincarranza.firebaseapp.com",
+    projectId: "chatedwincarranza",
+    storageBucket: "chatedwincarranza.appspot.com",
+    messagingSenderId: "849448005434",
+    appId: "1:849448005434:web:03cc35db51ebdad9cc6a03",
+    measurementId: "G-P1M3YFMH0Q"
+    });
